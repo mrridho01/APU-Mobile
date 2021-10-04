935b1c78f03ee5f86b55de99c93caa9bfc0db15c
MR <muhammadridho01@gmail.com>
2021-10-04 16:53:05 +0700

    initial commit

---
+++ src/pages/SplashScreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const splashScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default splashScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/pages/index.js
@@ -0,0 +1,1 @@
+import splashScreen from './SplashScreen';
